536cdd00b68f192c82407ab4807df1b5cbb2cf9f
David Belfort <davidedysonn@gmail.com>
2024-03-07 16:47:37 -0300

    feat: Algumas configurações criadas para uso futuro

---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+    <select v-model="selected">
+    <option disabled value="">Escolha um item</option>
+    </select>
+    <br>
+<span>Selecionado: {{ selected }}</span>
+</div>
+</template>
+
+<script>
+new Vue({
+    el: '...',
+    data: {
+      selected:''
+    }
+  });
+</script>
